597ec107086070d5889225be58d3a3b66b738f9d
Daniel Kraus <bovender@bovender.de>
2023-01-20 07:59:33 +0100

    Create temp file for upgrader script.

--- app/jobs/dora_web_upgrader/upgrade_job.rb
+++ app/jobs/dora_web_upgrader/upgrade_job.rb
@@ -9,7 +9,13 @@ module DoraWebUpgrader
       # overwrite uncommitted changes.
       if %w[production staging].include? Rails.env
         logger.warn '******** DoraWebUpgrader: PERFORMING APPLICATION UPGRADE ********'
-        exec "/usr/local/bin/upgrade-app.sh"
+        tempfile = Tempfile.new 'dora_web_upgrader_'
+        begin
+          tempfile.close
+          exec "/usr/local/bin/upgrade-app.sh '#{tempfile.path}'"
+        ensure
+          tempfile.unlink
+        end
       else
         logger.warn "Rails environment: #{Rails.env} -- not upgrading..."
       end
